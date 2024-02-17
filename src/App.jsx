aa9f9a29fac89055d947436412a163deac2606fb
seejn <seejnmaharjan@gmail.com>
2024-02-17 12:51:16 +0545

    refactor: move travely repo seejn -> kakapo

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+// import Header from "./components/Header";
+import LandingTemplate from "./templates/LandingTemplate";
+import Dashboard from "./templates/DashboardTemplate";
+
+function App() {
+    return (
+        <>
+            {/* <Header /> */}
+            {LandingTemplate}
+            <Dashboard />
+        </>
+    );
+}
+
+export default App;
---
+++ src/components/Header.jsx
@@ -0,0 +1,18 @@
+function Header(){
+  return(
+      <>
+  
+      <header class="bg-gray-900 text-white py-4">
+          <h1 class="text-center">Destination Travel Packages</h1>
+      </header>
+      <nav class="bg-gray-800 text-white py-2">
+        <a href="#" class="mx-2">Home</a>
+        <a href="#" class="mx-2">Packages</a>
+        <a href="#" class="mx-2">About Us</a>
+        <a href="#" class="mx-2">Contact</a>
+      </nav>
+      </>
+  )
+}
+
+export default  Header; 
