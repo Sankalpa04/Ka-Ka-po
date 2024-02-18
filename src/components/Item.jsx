fb4bc56a847b1d6129a0f5cb82bf607ebf068bea
sankalpa04 <sankalpa044@gmail.com>
2024-02-18 18:26:41 +0545

    Item component redesigned

--- src/components/Item.jsx
+++ src/components/Item.jsx
@@ -1,22 +1,19 @@
-import React from 'react'
+import React from "react";
 
 const Item = (props) => {
   return (
-    <div >
-        <div>
-            <h1>HIi</h1>{props.name}
+        <div class="bg-white rounded-lg shadow-md p-6">
+            <div class="text-lg font-semibold text-gray-900 mb-4 block">
+            <p>{props.name}</p>
+            </div>
+            <div>
+            <img class="mb-4 rounded-lg" src={props.image} alt="" />
+            </div>
+            <p class="text-gray-700">New Price: ${props.new_price}</p>
+            <p class="text-gray-700 mt-2 flex">Old Price:  $<h6 className="text-decoration-line: line-through">  {props.old_price}</h6></p>
+            <button class="bg-gray-900 text-white py-2 px-4 rounded cursor-pointer mt-2 ">View Details</button>
         </div>
-        <div>
-            <img src={props.image} alt="" />
-        </div>
-        <div>
-            {props.new_price}
-        </div>
-        <div>
-            {props.old_price}
-        </div>
-    </div>
-  )
-}
+  );
+};
 
-export default Item
+export default Item;
